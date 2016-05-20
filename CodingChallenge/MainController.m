d6b9b93eaa8f50ef0ae39fd611b10b11146742f1
Iván <me@ivancr.com>
2016-05-20 11:09:41 -0400

    Fix Localization strings

--- CodingChallenge/MainController.m
+++ CodingChallenge/MainController.m
@@ -8,6 +8,14 @@
 
 #import "MainController.h"
 
+
+#define kLocStringAudiobooks    NSLocalizedString(@"Top Audiobooks", @"Top Audiobooks")
+#define kLocStringBooks         NSLocalizedString(@"Top Paid Books", @"Top Paid Books")
+#define kLocStringiOSApps       NSLocalizedString(@"Top Paid iOS Apps", @"Top Paid iOS Apps")
+#define kLocStringiTunesU       NSLocalizedString(@"Top iTunes U Collections", @"Top iTunes U Collections")
+#define kLocStringMacApps       NSLocalizedString(@"Top Paid Mac Apps", @"Top Paid Mac Apps")
+#define kLocStringPodcasts      NSLocalizedString(@"Top Podcasts", @"Top Podcasts")
+
 @implementation MainController
 
 - (id)init {
@@ -20,13 +28,12 @@
                             @"toppaidmacapps",
                             @"toppodcasts"];
         
-        self.mediaTypesTitles = @{self.mediaTypes[0] : NSLocalizedString(@"Top Audiobooks", @"Top Audiobooks"),
-                                  self.mediaTypes[1] : NSLocalizedString(@"Top Paid Books", @"Top Paid Books"),
-                                  self.mediaTypes[2] : NSLocalizedString(@"Top Paid iOS Apps", @"Top Paid iOS Apps"),
-                                  self.mediaTypes[3] : NSLocalizedString(@"Top iTunes U Collections", @"Top iTunes U Collections"),
-                                  self.mediaTypes[4] : NSLocalizedString(@"Top Paid Mac Apps", @"Top Mac Apps"),
-                                  self.mediaTypes[5] : NSLocalizedString(@"Top Podcasts", @"Top Podcasts")};
-        
+        self.mediaTypesTitles = @{self.mediaTypes[0] : kLocStringAudiobooks,
+                                  self.mediaTypes[1] : kLocStringBooks,
+                                  self.mediaTypes[2] : kLocStringiOSApps,
+                                  self.mediaTypes[3] : kLocStringiTunesU,
+                                  self.mediaTypes[4] : kLocStringMacApps,
+                                  self.mediaTypes[5] : kLocStringPodcasts};
     }
     return self;
 }
